190f397c3d241d713bb8c155a3ecb0ee426a144a
Parth Thakkar <psthakkar2@gmail.com>
2025-07-15 17:59:50 +0530

    C variable basic understanding program.

---
+++ Concept_Wise/variable.c
@@ -0,0 +1,24 @@
+// Variables are containers for storing data values, like numbers and characters.
+
+// In c there are different types of variable like as follow.
+// -   int
+// -   float
+// -   char
+// -   & more.
+
+// Syntax
+// type variableName = value;
+
+// Example
+#include<stdio.h>
+int main(){
+    int myNum = 15;
+    printf("%d\n", myNum); // %d is format specifier(here use for the print integer value.), \n is use for the new line printing.
+
+    // Variable using declare.
+    int Num;
+    Num = 20;
+    printf("%d", Num);
+
+    return 0;
+}
