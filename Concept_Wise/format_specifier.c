a2bde123e506a355db1213e279e0963193848e4d
Parth Thakkar <psthakkar2@gmail.com>
2025-07-19 17:54:11 +0530

    unsigned Int and short value fromat specification.

--- Concept_Wise/format_specifier.c
+++ Concept_Wise/format_specifier.c
@@ -5,7 +5,7 @@
 int main(){
     // Let's start with the int example
     int myNum = 20;
-    printf("%d\n", myNum); // here the %d is format specifier which is use to for the print integer variable value.
+    printf("%d\n", myNum); // here the %d is format specifier which is use to for the print integer variable value(It also call signed Int).
 
     // Now let's go for the float value print
     float Temprature = 21.4;
@@ -26,6 +26,14 @@ int main(){
     // here the %lf is use for the print any double data type value printing. and if we want to print specific length number of value after point then we can add needed number in between % and lf with Syntax: %.3lf(here 3 is needed number it print only 3 length point after value.).
     printf("Double: %.4lf, %lf\n", doubleVal, doubleVal);
 
-    
+    // Unsigned Integer
+    unsigned int uIntVal = 4294961295;
+    // unsigned integer data format to reprsent is %u. and It use when we do not want to use the negative value.
+    printf("Unsigned Integer: %u \n", uIntVal);
+
+    // Short Integer value.
+    short shortVal = -32768;
+    printf("Short Integer value is: %hd \n", shortVal); // short integer value represent by %hd format. It range between –32,768 to 32,767. more negative use default int value.
+
     return 0;
 }
